fa7ac9d00de41b40c718f23457efd005cf53a805
unknown <winring86@gmail.com>
2026-07-24 07:04:09 -0700

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: '/',
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/usmnt-tracker/' : '/',
+// });
